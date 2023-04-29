e28aa2d49cdf898b606ae899ccaa2b780b544776
Yannis Caff <yadr14@hotmail.com>
2023-04-29 16:39:42 +0100

    Started blog container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppButton from "./Components/button";
+import BlogContainer from "./Components/blogContainer";
 
 function App() {
   return (
     <div className="App">
       <AppButton>Button</AppButton>
+      <BlogContainer>blog container</BlogContainer>
     </div>
   );
 }
--- src/Components/blogContainer.jsx
+++ src/Components/blogContainer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 const BlogContainer = () => {
-  return;
+  return <Box></Box>;
 };
+
+export default BlogContainer;
