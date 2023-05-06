d829031daad59d0b83fe52d59d873cbbc5d6235b
Yannis Caff <yadr14@hotmail.com>
2023-05-06 23:07:56 +0100

    add responsive grids when multple blog cards are displayed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import AppButton from "./Components/button";
 import BlogCard from "./Components/blogCard";
+import AllBlogs from "./Components/allBlogs";
 
 function App() {
   return (
     <div className="App">
       <div className="panel">
-        <BlogCard>blog card</BlogCard>
+        <AllBlogs></AllBlogs>
       </div>
     </div>
   );
