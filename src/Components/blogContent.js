74129115b951c688f52aea1aa84818e8d0318648
Yannis Caff <yadr14@hotmail.com>
2023-05-30 23:26:30 +0100

    reformat so blog post variations can be added

---
+++ src/Components/blogContent.js
@@ -0,0 +1,18 @@
+
+
+const blogContent = {
+  blog1 : {
+    header : "Is rock skiing the greastest pastime known to man? Yes! And here's why.",
+    readTime : "10m read time",
+    image : ,
+    content : ""
+  },
+  blog2 : {
+    header : "",
+    readTime : "",
+    image : ,
+    content : ""
+  },
+};
+
+export default blogContent;
