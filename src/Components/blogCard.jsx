23b7f632d82a0a3feebeb5958899e80fa971f3a6
Yannis Caff <yadr14@hotmail.com>
2023-06-07 00:06:24 +0100

    add content to sidebar

--- src/Components/blogCard.jsx
+++ src/Components/blogCard.jsx
@@ -13,7 +13,7 @@ const cardImage = imageMain;
 const posterImage = imagePoster;
 const boxArrow = <BsBoxArrowUpRight className="box-arrow" />;
 
-const BlogCard = ({title, readTime, image}) => {
+const BlogCard = ({ title, readTime, image }) => {
   return (
     <div className="blog-card-container">
       <div className="inner-container">
@@ -24,17 +24,13 @@ const BlogCard = ({title, readTime, image}) => {
           <HiEllipsisVertical className="ellipsis" />
         </div>
         <div className="card-title-container">
-          <h1 className="card-title">
-          {title}
-          </h1>
+          <h1 className="card-title">{title}</h1>
         </div>
         <div className="read-time-contianer">
-          <p className="read-time">
-            {readTime}
-          </p>
+          <p className="read-time">{readTime}</p>
         </div>
         <div className="card-image-container">
-        <img className="card-image" src={image} />
+          <img className="card-image" src={image} />
         </div>
         <div className="card-footer">
           <TbArrowBigUp className="up-arrow-icon" />
@@ -49,5 +45,3 @@ const BlogCard = ({title, readTime, image}) => {
 };
 
 export default BlogCard;
-
-
--- src/Components/sideBar.jsx
+++ src/Components/sideBar.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { BsFire } from "react-icons/bs";
 
 const SideBar = () => {
-  return <div className="side-bar"></div>;
+  return (
+    <div className="side-bar">
+      <div className="side-bar-container">
+        <div className="main-feed-container">
+          <h2>Main Feed</h2>
+        </div>
+        <div className="discover-container">
+          <h2>Discover</h2>
+        </div>
+        <div className="disc-menu">
+          <div className="popular-container">
+            <BsFire className="popular-fire-icon" />
+            <h2 className="popular">Popular</h2>
+          </div>
+          <div className="most-upvoted">
+            <h2>Most Upvoted</h2>
+          </div>
+          <div className="best-disc">
+            <h2>Best Discussions</h2>
+          </div>
+          <div>
+            <h2 className="search">Search</h2>
+          </div>
+        </div>
+        <div className="manage-container">
+          <h2>Manage</h2>
+        </div>
+        <div className="manage-menu">
+          <div className="bookmarks">
+            <h2>Bookmarks</h2>
+          </div>
+          <div className="read-history">
+            <h2>Reading History</h2>
+          </div>
+          <div className="customize">
+            <h2>Customize</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
