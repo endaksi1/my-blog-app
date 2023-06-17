132514b55a816bf4408cf635fec3aa3f040ade18
Yannis Caff <yadr14@hotmail.com>
2023-06-18 00:18:58 +0100

    add modal with temp button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 
 import FrontPage from "./Pages/frontPage";
+import Modal from "./Pages/modal";
 
 function App() {
   return (
     <div className="App">
       <div className="panel">
-        <FrontPage></FrontPage>
+        <FrontPage />
+        <Modal />
       </div>
     </div>
   );
